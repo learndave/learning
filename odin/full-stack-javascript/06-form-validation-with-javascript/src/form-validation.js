071daf39f2cca533e7dc2d14068ca585304a58b2
learndave <franscabantingdum+learning@gmail.com>
2022-04-18 23:34:44 +0800

    learn Constraint Validation API

--- odin/full-stack-javascript/06-form-validation-with-javascript/src/form-validation.js
+++ odin/full-stack-javascript/06-form-validation-with-javascript/src/form-validation.js
@@ -2,29 +2,50 @@ const form  = document.querySelector('.form-container');
 
 const username = document.querySelector('input.username');
 const email = document.querySelector('input.email');
-const phone = document.querySelector('input.phone');
-
-// username.addEventListener('input', performUsernameValidityCheck);
-// email.addEventListener('input', performUsernameValidityCheck);
-// phone.addEventListener('input', performPhoneValidityCheck);
+const password = document.querySelector('input.password');
+const country = document.querySelector('input.country');
+const zipcode = document.querySelector('input.zipcode');
 
 form.addEventListener('submit', (e) => performFormValidityCheck(e));
 
+
+
+const initiateUsernameConstraint = () => {
+    username.minLength = 6;
+    username.maxLength = 30;
+    username.pattern = "^[a-zA-Z]+[a-zA-Z0-9]*$";
+    username.required = true;
+}
+
 function showError() {
-    if (email.validity.typeMismatch) {
+    if (username.validity.tooShort) {
+        username.setCustomValidity('Your username should contain at least 4 characters.');
+        username.reportValidity();
+    } else if (username.validity.tooLong) {
+        username.setCustomValidity('Your username should contain at most 20 characters.');
+        username.reportValidity();
+    } else if (username.validity.patternMismatch) {
+        username.setCustomValidity('Your username should contain only letters and numbers. It should not start with a number.');
+        username.reportValidity();
+    } else if (email.validity.typeMismatch) {
         email.setCustomValidity('Input a valid email.');
         email.reportValidity();
-    }
-    if (email.validity.tooShort) {
+    } else if (email.validity.tooShort) {
         email.setCustomValidity(`Your email should be at least ${email.minLength} characters. You entered ${email.value.length} so far`);
         email.reportValidity();
     }
 }
 
 function performFormValidityCheck(e) {
-    console.log(email)
-    if(!username.validity.valid || !email.validity.valid || !phone.validity.valid ) {
+    if(!username.validity.valid ||
+        !email.validity.valid ||
+        !password.validity.valid ||
+        !country.validity.valid ||
+        !zipcode.validity.valid) {
         showError();
         e.preventDefault();
     }
 }
+
+
+initiateUsernameConstraint();
