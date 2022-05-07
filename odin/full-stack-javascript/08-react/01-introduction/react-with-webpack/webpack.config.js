a0bed91bb3d44c4b931afe2f698f1efdbde40f6a
learndave <franscabantingdum+learning@gmail.com>
2022-05-07 16:56:25 +0800

    setup webpack

---
+++ odin/full-stack-javascript/08-react/01-introduction/react-with-webpack/webpack.config.js
@@ -0,0 +1,27 @@
+import { resolve } from "path";
+
+const DIST_DIR = resolve(__dirname, "dist");
+const SRC_DIR = resolve(__dirname, "src");
+
+const config = {
+    entry: SRC_DIR + "/app/index.js",
+    output: {
+        path: DIST_DIR + "/app",
+        filename: "bundle.js",
+        publicPath: "/app/"
+    },
+    modules: {
+        loaders: [
+            {
+                test: /\.js?/,
+                include: SRC_DIR,
+                loader: "babel-loader",
+                query: {
+                    presets: ["react", "es2015", "stage-2"]
+                }
+            }
+        ]
+    }
+}
+
+export default config;
